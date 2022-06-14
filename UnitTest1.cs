c429935485c4170fb130bb7fbcd865f2756295fa
Ahmed Faraz Ali <farazali506@gmail.com>
2022-06-15 00:42:58 +0500

    Unit Testing

---
+++ UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Bank;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// https://github.com/AhmedFarazAli
+
+namespace BankTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //https://github.com/AhmedFarazAli
+        }
+
+        [TestMethod]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            
+            // https://github.com/AhmedFarazAli
+
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Ahmed Faraz Ali", beginningBalance);
+
+            //https://github.com/AhmedFarazAli
+
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+        }
+    }
+}
